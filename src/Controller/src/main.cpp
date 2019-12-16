4652ebdd696b017da5f07be6410ebb37f0d8041e
LHospitalLKY <lkywxd@qq.com>
2019-12-16 19:40:50 +0800

    增加了自定义的msg: master_control

--- src/Controller/src/main.cpp
+++ src/Controller/src/main.cpp
@@ -8,6 +8,7 @@
 #include <ros/ros.h>
 #include <std_msgs/Float32.h>
 #include <std_msgs/Bool.h>
+#include <Controller/master_control.h>
 #include <libconfig.h++>
 
 using namespace libconfig;
@@ -18,9 +19,10 @@ class SubscribeAndPublish
 public:  
     SubscribeAndPublish(double kp, double kd, double ki) {
         pid_ = new PID_Controller(kp, kd, ki);
+        is_stop_ = 0;
         count = 0;
         //Topic you want to publish  
-        pub_ = n_.advertise<std_msgs::Float32>("/angle_pub", 1000);  
+        pub_ = n_.advertise<Controller::master_control>("/angle_pub", 1000);  
 
         //Topic1 you want to subscribe  
         sub_ = n_.subscribe("offset_from_lanecenter", 10, &SubscribeAndPublish::callback1, this); 
@@ -42,9 +44,15 @@ public:
     void callback1(const std_msgs::Float32::ConstPtr& msg1) {  
         ROS_INFO("Steering Angle: [%f]", msg1 -> data); 
         double angle = pid_ -> compute(msg1 -> data);
-        angle_.data = angle;
-        pub_.publish(angle_);
-        ROS_INFO("%f", angle_.data); 
+        ctrl_msg_.servo_ang = (int32_t)angle;
+        ctrl_msg_.speed = 0.25;
+        // 如果is_stop_ == 0，则让小车停止行动
+        if(is_stop_ == 1) {
+            ctrl_msg_.servo_ang = 0;
+            ctrl_msg_.speed = 0.0;
+        }
+        pub_.publish(ctrl_msg_);
+        ROS_INFO("angle: %d, speed: %f", ctrl_msg_.servo_ang, ctrl_msg_.speed); 
         ++count; 
     }  
     // TODO: 障碍物识别、交通灯识别的预留回调函数
@@ -56,8 +64,11 @@ private:
     ros::Subscriber sub_;
     ros::Subscriber sub2_;
     // TODO: 将这个msg换掉，添加speed信息 
-    std_msgs::Float32 angle_;
+    Controller::master_control ctrl_msg_;
     int count; 
+
+    // 是否停止的flag
+    int is_stop_;
 
     // PID控制器
     PID_Controller *pid_;
